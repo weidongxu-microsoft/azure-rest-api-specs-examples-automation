ee50288a0322cb22d373fe3090f289571a2b445f
Weidong Xu <weidxu@microsoft.com>
2021-09-14 17:04:13 +0800

    fix

--- java/main.py
+++ java/main.py
@@ -76,7 +76,7 @@ def get_java_example_method(lines: List[str], start: int) -> JavaExampleMethodCo
             original_file = line.strip()[len(original_file_key):].lower()
         elif line.startswith('    public static void '):
             # begin of method
-            java_example_method.example_reference = original_file
+            java_example_method.example_relative_path = original_file
             java_example_method.line_start = index
         elif line.startswith('    }'):
             # end of method
