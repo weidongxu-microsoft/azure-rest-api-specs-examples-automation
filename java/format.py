c5723beed30769346582cf6b179286d60c4cf873
Weidong Xu <weidxu@microsoft.com>
2021-09-14 15:01:41 +0800

    use temp file for java format, to enable concurrency

--- java/format.py
+++ java/format.py
@@ -1,6 +1,7 @@
-import os
+from os import path
 import subprocess
 import dataclasses
+import tempfile
 import logging
 
 
@@ -11,9 +12,11 @@ class CompletedJavaCode:
 
 
 class JavaFormat:
+    tmp_path: str
     maven_path: str
 
-    def __init__(self, maven_path: str):
+    def __init__(self, tmp_path: str, maven_path: str):
+        self.tmp_path = tmp_path
         self.maven_path = maven_path
 
     def build(self):
@@ -23,17 +26,21 @@ class JavaFormat:
         subprocess.check_call(cmd, cwd=self.maven_path)
 
     def format(self, java_example: str) -> CompletedJavaCode:
-        os.environ['JAVA_CODE'] = java_example
+        with tempfile.TemporaryDirectory(dir=self.tmp_path) as tmp_dir_name:
+            tmp_filepath = path.join(tmp_dir_name, 'tmp.java')
 
-        cmd = ['java', '-jar', 'target/javaformat-1.0.0-beta.1-jar-with-dependencies.jar']
-        # logging.info('Format java code')
-        # logging.info('Command line: ' + ' '.join(cmd))
-        result = subprocess.run(cmd, capture_output=True, text=True, encoding='utf-8', cwd=self.maven_path)
+            with open(tmp_filepath, 'w', encoding='utf-8') as f:
+                f.write(java_example)
 
-        if result.returncode:
-            return CompletedJavaCode(result.returncode)
-        else:
-            formatted_code = result.stdout
-            if formatted_code.endswith('\n\n'):
-                formatted_code = formatted_code[:-1]
-            return CompletedJavaCode(result.returncode, formatted_code)
+            cmd = ['java', '-jar', 'target/javaformat-1.0.0-beta.1-jar-with-dependencies.jar', tmp_filepath]
+            # logging.info('Format java code')
+            # logging.info('Command line: ' + ' '.join(cmd))
+            result = subprocess.run(cmd, capture_output=True, text=True, encoding='utf-8', cwd=self.maven_path)
+
+            if result.returncode:
+                return CompletedJavaCode(result.returncode)
+            else:
+                formatted_code = result.stdout
+                if formatted_code.endswith('\n\n'):
+                    formatted_code = formatted_code[:-1]
+                return CompletedJavaCode(result.returncode, formatted_code)
