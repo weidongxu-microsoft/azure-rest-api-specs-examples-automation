4876ba4f5c23dea1944d07d78f6db105fb87bb88
Weidong Xu <weidxu@microsoft.com>
2022-05-26 16:54:15 +0800

    Support skip processed libs (#17)
    * support database.query_releases
    
    * add option of skip-processed

--- automation/database.py
+++ automation/database.py
@@ -3,10 +3,14 @@ import sqlite3
 import logging
 from typing import List
 
+from models import Release
+
 
 SCRIPT_QUERY_RELEASE = '''select name from release where name = ? and language = ?'''
 SCRIPT_INSERT_RELEASE = '''insert or replace into release (name, language, tag, package, version, date_epoch) values (?, ?, ?, ?, ?, ?)'''
 SCRIPT_INSERT_FILE = '''insert or replace into file (file, release_id) values (?, ?)'''
+
+SCRIPT_QUERY_RELEASE_DETAIL = '''select tag, package, version, date_epoch from release where language = ?'''
 
 
 class Database:
@@ -17,6 +21,9 @@ class Database:
 
     def new_release(self, name: str, language: str, tag: str, package: str, version: str, date: datetime,
                     files: List[str]) -> bool:
+        # add a new release and all the example files
+        # return false, if release already exists in DB
+
         date_epoch = int(date.timestamp())
         try:
             with sqlite3.connect(self.database_path) as conn:
@@ -41,3 +48,21 @@ class Database:
         except sqlite3.Error as error:
             logging.error(f'Database error: {error}')
             return False
+
+    def query_releases(self, language: str) -> List[Release]:
+        # query processed releases
+
+        try:
+            releases = []
+            with sqlite3.connect(self.database_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute(SCRIPT_QUERY_RELEASE_DETAIL, (language,))
+                release_rows = cursor.fetchall()
+                if release_rows:
+                    for row in release_rows:
+                        date = datetime.fromtimestamp(row[3])
+                        releases.append(Release(row[0], row[1], row[2], date))
+            return releases
+        except sqlite3.Error as error:
+            logging.error(f'Database error: {error}')
+            return []
--- automation/test_database.py
+++ automation/test_database.py
@@ -33,3 +33,39 @@ class TestDatabase(unittest.TestCase):
         succeeded = database.new_release(
             name, language, 'release_tag1', 'mock-sdk', '1.0.0-beta.1', datetime.now(), ['file3'])
         self.assertFalse(succeeded)
+
+    def test_query(self):
+        # init database
+        try:
+            os.remove('test.db')
+        except OSError:
+            pass
+        with sqlite3.connect('test.db') as conn:
+            script1(conn.cursor())
+
+        # populate db
+        database = Database('test.db')
+        succeeded = database.new_release(
+            'com.azure.resourcemanager:mock-sdk:1.0.0-beta.1',
+            'java', 'release_tag1', 'mock-sdk', '1.0.0-beta.1', datetime.now(), ['file1', 'file2'])
+        self.assertTrue(succeeded)
+
+        succeeded = database.new_release(
+            'com.azure.resourcemanager:mock-sdk:1.0.0-beta.2',
+            'java', 'release_tag2', 'mock-sdk', '1.0.0-beta.2', datetime.now(), ['file3'])
+        self.assertTrue(succeeded)
+
+        succeeded = database.new_release(
+            'github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mock-sdk-go@v0.5.0',
+            'go', 'release_tag3', 'mock-sdk-go', '0.5.0', datetime.now(), ['file1_go'])
+        self.assertTrue(succeeded)
+
+        # query
+        releases = database.query_releases('java')
+        self.assertEqual(2, len(releases))
+
+        releases = database.query_releases('go')
+        self.assertEqual(1, len(releases))
+        self.assertEqual('release_tag3', releases[0].tag)
+        self.assertEqual('mock-sdk-go', releases[0].package)
+        self.assertEqual('0.5.0', releases[0].version)
